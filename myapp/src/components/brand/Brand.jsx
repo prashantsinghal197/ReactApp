692c26f293a335e7f89c6091ec0e8505498f99a8
Prashant Singhal <singhalprashant197@gmail.com>
2023-12-21 18:29:44 +0530

    Created an app

---
+++ myapp/src/components/brand/Brand.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import './brand.css';
+import {google, slack, atlassian, dropbox, shopify,} from './import.js'; 
+const Brand = () => {
+  return (
+    <div className="gpt3__brand section__padding">
+    <div>
+      <img src={google} alt='Google'/>
+    </div>
+    <div>
+      <img src={slack} alt='Slack'/>
+    </div>
+    <div>
+      <img src={atlassian} alt='Atlassian'/>
+    </div>
+    <div>
+      <img src={dropbox} alt='DropBox'/>
+    </div>
+    <div>
+      <img src={shopify} alt='Shopify'/>
+    </div>
+  </div>
+  )
+}
+
+export default Brand
